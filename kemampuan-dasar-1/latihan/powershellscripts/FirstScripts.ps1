d0bd1fde1d7649ce76e9ae313dc93a08c26edea2
Robi Dahariansyah <robidahariansyah8@gmail.com>
2019-11-08 11:31:02 +0700

    first commit

---
+++ kemampuan-dasar-1/latihan/powershellscripts/FirstScripts.ps1
@@ -0,0 +1,6 @@
+1. PS D:\praxis-academy\kemampuan-dasar-1\latihan> New-Item -Path powershellscripts -ItemType directory
+2. PS D:\praxis-academy\kemampuan-dasar-1\latihan> Get-Item
+   cmdlet Get-Item at command pipeline position 1
+   Supply values for the following parameters:
+   Path[0]: powershellscripts
+3. PS D:\> Get-Date
