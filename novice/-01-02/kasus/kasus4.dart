182b33cd645a3ade2c88ce9d395cd68d25b738d8
Robi Dahariansyah <robidahariansyah8@gmail.com>
2019-11-13 09:15:32 +0700

    memperbaiki kasus4.dart

--- novice/-01-02/kasus/kasus4.dart
+++ novice/-01-02/kasus/kasus4.dart
@@ -1,19 +1,19 @@
+// fungsi menacri nilai min dan max
+min_max(a) {
+  var min = a[0];
+  var max = a[0];
+  for (var i = 1; i <= a.length - 1; i++) {
+    if (a[i] < min) {
+      min = a[i];
+    }
+    if (a[i] > max) {
+      max = a[i];
+    }
+  }
+  print("$min $max");
+}
+
 main() {
   var A = [12, 23, 22, 10, 2];
-
-  min_max(a) {
-    var min = a[0];
-    var max = a[0];
-    for (var i = 1; i <= A.length - 1; i++) {
-      if (a[i] < min) {
-        min = a[i];
-      }
-      if (a[i] > max) {
-        max = a[i];
-      }
-    }
-    print("$min $max");
-  }
-
   min_max(A);
 }
